09ecd311c9fd6ac96e7b7e46a375cdc3b312e4d2
Parker Hodge <parkerhodge@Parkers-MacBook-Pro.local>
2020-02-24 10:48:57 -0600

    added Dolly Purrton to index.html added associated dialog to dialog.js

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -49,6 +49,15 @@ const initializeDetailButtonEvents = () => {
             theDialog.showModal()
         }
     )
+
+     // Show Dolly's details when the button is clicked
+     document.querySelector("#button--dolly").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--dolly")
+            theDialog.showModal()
+        }
+    )
 }
 
 export default initializeDetailButtonEvents
